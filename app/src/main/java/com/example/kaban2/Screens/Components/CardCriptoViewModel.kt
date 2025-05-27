f02a8a7ed3933694010f4a502d4ded1b75c07466
AnastaSiaBog <bgtkva2006@gmail.com>
2025-05-28 00:02:14 +0300

    Change CardCriptoViewModel.kt

--- app/src/main/java/com/example/kaban2/Screens/Components/CardCriptoViewModel.kt
+++ app/src/main/java/com/example/kaban2/Screens/Components/CardCriptoViewModel.kt
@@ -92,7 +92,7 @@ class CardCriptoViewModel : ViewModel() {
                                 }}
                             .decodeSingle<User_cripto>()
 
-                        if (profileResult3.quantity > 0) {
+if (profileResult3.quantity > 0) {
                             Log.d("quantity", profileResult3.quantity.toString())
                             supabase.from("users_have_cryptocurrency").update(
                                 {
@@ -197,7 +197,7 @@ class CardCriptoViewModel : ViewModel() {
                                     }
                                 }
 
-                                val percentChange = (Random.nextInt(-5, 6)) * 0.03  // от -15% до +15%
+val percentChange = (Random.nextInt(-5, 6)) * 0.03  // от -15% до +15%
                                 val cost = cripto.cost + percentChange * 0.03 * cripto.cost
 
                                 supabase.from("cryptocurrency").update(
@@ -301,7 +301,7 @@ class CardCriptoViewModel : ViewModel() {
                             }}
                         .decodeList<User_cripto>()
 
-                    _criptoList.value = profileResult4
+_criptoList.value = profileResult4
                 } else {
                     showMessage("У вас нет такой криптовалюты")
                 }
